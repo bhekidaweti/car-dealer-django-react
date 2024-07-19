820dae0633b13bf92fd4818ebd476136c8d7290f
bhekidaweti <bheki.daweti@gmail.com>
2024-07-19 14:17:55 +0200

    added background  color

--- car_dealer_backend/dealership/models.py
+++ car_dealer_backend/dealership/models.py
@@ -7,17 +7,31 @@ class Car(models.Model):
     ("AUTOMATIC", "AUTOMATIC"), 
     ("MANUAL", "MANUAL"), 
     ] 
+    FUEL_CHOICES = [ 
+    ("PETROL", "PETROL"), 
+    ("DIESEL", "DIESEL"), 
+    ("ELECTRIC", "ELECTRIC"), 
+    ("HYBRID", "HYBRID"), 
+    ]
+    VEHICLE_CHOICES = [ 
+    ("SUV", "SUV"), 
+    ("TRUCK", "TRUCK"),
+    ("BUS", "BUS"),
+    ("BAKKIE", "BAKKIE"),
+    ("SEDAN", "SEDAN"),
+    ("HATCHBACK", "HATCHBACK"),
+    ] 
     image = models.ImageField(upload_to='cars/')
     description = models.CharField(max_length=1000)
     year = models.IntegerField()
     mileage = models.IntegerField(null=True)
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
-    vehicle_type = models.CharField(max_length=50)
+    vehicle_type = models.CharField(max_length=50, choices=VEHICLE_CHOICES, default='TRUCK')
     seats = models.IntegerField()
     doors = models.IntegerField()
     transmission_type = models.CharField(max_length=50, choices= TRANSMISSION_CHOICES, default= 'MANUAL')
-    fuel_type = models.CharField(max_length=50)
+    fuel_type = models.CharField(max_length=50, choices=FUEL_CHOICES, default= 'PETROL')
     is_sold = models.BooleanField(default=False)
 
     def __str__(self):
--- car_dealer_frontend/src/components/Home.js
+++ car_dealer_frontend/src/components/Home.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCar } from '@fortawesome/free-solid-svg-icons';
 import './Home.css';
 import heroImage from '../assets/hero-image.jpg';
-import footerImage from '../assets/footer-image.png';
+import footerImage from '../assets/footer-bg-image.png';
 
 const Home = () => {
   return (
@@ -12,17 +12,13 @@ const Home = () => {
       <div className="hero-image" style={{ backgroundImage: `url(${heroImage})` }}>
         <div className="hero-text">
           <h1>Welcome to BK Motorsport</h1>
-          <p>Your destination for legendery classic cars</p>
+          <p><b>Your destination for legendery classic cars</b></p>
           <Link to="/cars" className="hero-button"><FontAwesomeIcon icon={faCar} />View Car List</Link>
         </div>
       </div>
-      <div className="footer-image" style={{ backgroundImage: `url(${footerImage})` }}>
+      
       <div className="navbar-logo">
         <img src={footerImage} alt="bk motorsport" />
-      </div>
-        <div className="footer-text">
-          <p>bk motorsport 2024&#169; </p>
-        </div>
       </div>
     </div>
   );
